68471e7c667419ae88fe27d486a8aee3ba2be36f
İsmail Baran <ismailbaran04@gmail.com>
2025-02-11 22:34:54 +0300

    about için model oluşturuldu

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { About } from '../_models/about';
 
 @Component({
   selector: 'app-about',
@@ -7,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+  about : About = {id:1, title:"Başlık", description:"lorem ipum dolor sit amet"}
+  about2 : About = {id:2, title:"Başlı 2", description:"lorem ipum dolor sit amet constructıtıttııtıtıt"}
+  about3 : About = {id:3, title:"Başlık 3", description:"lorem ipum dolor sit amet constereererere"}
+  about4 : About = {id:4, title:"Başlık 4", description:"lorem ipum dolor sit amet constereererere"}
 
+  abouts : About[] = [this.about, this.about2, this.about3, this.about4]
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { HomeComponent } from './home/home.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { MenuComponent } from './menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes =
+[
+  {//Main route yapılandırması
+  path:'', component: MainLayoutComponent,
+  children: [
+    { path:'about', component:AboutComponent},
+    { path:'contact', component:ContactComponent},
+    { path:'menu', component:MenuComponent}
+  ]
+},
+//Admin route yapılandırması
+{
+  path:'admin', component:AdminLayoutComponent,
+
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
